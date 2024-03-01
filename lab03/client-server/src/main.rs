e705db0b38402de736bdea01383e9ac0b156edb9
metametamoon <sorvinlevs@gmail.com>
2024-03-01 11:31:51 +0300

    Implemented the client server as well

---
+++ lab03/client-server/src/main.rs
@@ -0,0 +1,40 @@
+use std::{fmt::format, io::{stdin, Read, Write}, net::TcpStream, str::from_utf8};
+
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    let defaul_host = "127.0.0.1".to_owned();
+    let defaul_port = "1337".to_owned();
+    let defaul_file_name = "b.txt".to_owned();
+    
+    let host = args.get(1).unwrap_or(&defaul_host);
+    let port = args.get(2).unwrap_or(&defaul_port);
+    let file_name = args.get(3).unwrap_or(&defaul_file_name);
+    
+    let addr = format!("{}:{}", host, port);
+    println!("Connecting to {}", addr);
+    let mut stream = TcpStream::connect(addr).unwrap();
+
+    let query = format!("GET /{} HTTP/1.1\r\n\r\n", file_name);
+    stream.write_all(query.as_bytes()).unwrap();
+    let mut response =  Vec::<u8>::new();
+    loop {
+        let mut buf = [0u8; 128];
+        let read = stream.read(&mut buf).unwrap();
+        if read == 0 {
+            break;
+        } else {
+            response.extend(&buf[0..read]);
+            println!("Received another {read} bytes");
+            if read < 128 {
+                break;
+            }
+        }
+    }
+    let response_str = from_utf8(&response);
+    if let Ok(str) = response_str {
+        println!("{}", str);
+    } else {
+        println!("Not utf8; too bad!");
+    }
+    stream.shutdown(std::net::Shutdown::Both).unwrap();
+}
--- lab03/file-lookup-server/src/main.rs
+++ lab03/file-lookup-server/src/main.rs
@@ -5,12 +5,11 @@ use std::{
     io::{BufRead, BufReader, Read, Write},
     net::*,
     path::Path,
-    thread::{self, sleep, JoinHandle}, time::Duration,
+    thread::{self, sleep, JoinHandle},
+    time::Duration,
 };
 
 use crate::threadpool::ThreadPool;
-
-
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -31,36 +30,46 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // println!("Connection received; waiting 15s");
-    // sleep(Duration::from_secs(15));
-    let reader = BufReader::new(&mut stream);
-    let request = reader
-        .lines()
-        .map(|x| x.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<_>>();
-    let main_part = &request[0];
-    let file_name = main_part
-        .strip_prefix("GET /")
-        .and_then(|x| x.strip_suffix("HTTP/1.1"));
-    if let Some(file_name) = file_name {
-        if !file_name.starts_with("favicon") {
-            let path = Path::new(file_name);
-            if let Ok(mut file) = File::open(path) {
-                let status = "HTTP/1.1 200 OK";
-                let mut contents: String = String::new();
-                file.read_to_string(&mut contents).unwrap();
-                let length = contents.len();
-                let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
-                stream.write_all(response.as_bytes()).unwrap();
-                return;
-            }
-        } else {
-            let status = "HTTP/1.1 200 OK";
-            stream.write_all(status.as_bytes()).unwrap();
-            return;
+    loop {
+        let reader = BufReader::new(&mut stream);
+        let request = reader
+            .lines()
+            .map(|x| x.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect::<Vec<_>>();   
+        if request.len() == 0 {
+            continue;
         }
+        let main_part = &request[0];
+        println!("main_part={main_part}");
+        let file_name = main_part
+            .trim()
+            .strip_prefix("GET /")
+            .and_then(|x| x.strip_suffix("HTTP/1.1"));
+        println!("{file_name:?}");
+        if let Some(file_name) = file_name {
+            if !file_name.starts_with("favicon") {
+                let path = Path::new(file_name);
+                if let Ok(mut file) = File::open(path) {
+                    let status = "HTTP/1.1 200 OK";
+                    let mut contents: String = String::new();
+                    file.read_to_string(&mut contents).unwrap();
+                    let length = contents.len();
+                    let response =
+                        format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+                    println!("{}", response);
+                    stream.write_all(response.as_bytes()).unwrap();
+                    continue;;
+                }
+            } else {
+                let status = "HTTP/1.1 200 OK";
+                println!("{}", status);
+                stream.write_all(status.as_bytes()).unwrap();
+                continue;
+            }
+        }
+        let status = "HTTP/1.1 404 Not Found";
+        println!("{}", status);
+        stream.write_all(status.as_bytes()).unwrap();
     }
-    let status = "HTTP/1.1 404 Not Found";
-    stream.write_all(status.as_bytes()).unwrap();
 }
